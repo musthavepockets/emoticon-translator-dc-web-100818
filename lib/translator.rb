2a7a6f5ada7210d6b65316e68fff08e1e61a4a14
cd <coraldarling@outlook.com>
2018-10-06 23:02:31 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,8 @@
 require 'yaml'
 
 def load_library
-YAML.load_file("./lib/emoticons.yml")
-#puts thing.inspect
+  YAML.load_file("./lib/emoticons.yml")
+  thing.inspect
 end
 
 def get_japanese_emoticon
