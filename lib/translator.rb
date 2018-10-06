7c70412754be4c3c31a15a0c2282fc21ea76d4f5
cd <coraldarling@outlook.com>
2018-10-06 23:52:27 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,7 +2,7 @@ require 'yaml'
 
 def load_library
   emoticons = {"get_meaning" => {}, "get_emoticon" => {}}
-  YAML.load_file('emoticons.yml')
+  YAML.load_file('emoticons.yml').each do 
   emoticons[get_meaning][0]
   meaning[1]
   emoticons.inspect
