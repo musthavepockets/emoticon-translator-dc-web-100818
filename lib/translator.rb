34d032ef5317bdf65915f5fdc357abcfc109888b
cd <coraldarling@outlook.com>
2018-10-07 00:20:04 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,8 +4,8 @@ def load_library(path)
   patn = './lib/emoticons.yml'
   emoticons = {"get_meaning" => {}, "get_emoticon" => {}}
   YAML.load_file('./lib/emoticons.yml').each do |meaning, japanese, english|
-    emoticons[get_meaning][japanese] = meaning
-    emoticons[get_emoticon][english] = japanese
+    emoticons["get_meaning"][japanese] = meaning
+    emoticons["get_emoticon"][english] = japanese
   end
   emoticons
 end
