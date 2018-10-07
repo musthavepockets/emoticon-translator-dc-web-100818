8845c42e69d94bba20314e6d9851317c719cdb1b
cd <coraldarling@outlook.com>
2018-10-07 00:38:26 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -10,7 +10,7 @@ def load_library(path)
   emoticons
 end
 
-def get_japanese_emoticon(emoticons, emoticon)
+def get_japanese_emoticon(load_library, emoticon)
   
   #get_meaning.fetch(emoticon)
 end
