7f1291db13275fe5a4bf350c2dda521a656bd80e
cd <coraldarling@outlook.com>
2018-10-07 01:07:46 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,7 +11,8 @@ def load_library(path)
 end
 
 def get_japanese_emoticon(path, emoticon)
-  load_library('./lib/emoticons.yml')[emoticons][get_japanese_emoticon][emoticon]
+  load_library('./lib/emoticons.yml')
+  [emoticons][get_japanese_emoticon][emoticon]
    
   if  japanese_emoticon = nil
     "Sorry, that emoticon was not found"
